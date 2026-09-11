832e320a8300f5b2bec695e4dae94099bdf22a16
FelixFa06 <felfa1414666@gmail.com>
2026-09-11 15:30:16 +0800

    feat: initialize hamming.v

---
+++ lab03/hamming.v
@@ -0,0 +1,14 @@
+module hamming(
+	input  [6:0] code,
+	output reg [6:0] correct,
+	output [2:0] parity
+	);
+
+assign parity = {code[3],code[1],code[0]};
+always @(*) begin
+    correct = code;
+    if (|parity)
+        correct[parity-1 ]= correct[parity-1]^1;
+end
+
+endmodule
