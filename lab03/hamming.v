06d4a65bf7fc10b9a284b17c03e4d50601939e53
FelixFa06 <felfa1414666@gmail.com>
2026-09-11 15:36:39 +0800

    fix: fix the bug in hamming.v,finish oj of lab03

--- lab03/hamming.v
+++ lab03/hamming.v
@@ -4,11 +4,11 @@ module hamming(
 	output [2:0] parity
 	);
 
-assign parity = {code[3],code[1],code[0]};
+assign parity = {code[3]^code[4]^code[5]^code[6],code[1]^code[2]^code[5]^code[6],code[0]^code[2]^code[4]^code[6]};
 always @(*) begin
     correct = code;
     if (|parity)
-        correct[parity-1 ]= correct[parity-1]^1;
+        correct[parity-1]= correct[parity-1]^1;
 end
 
 endmodule
