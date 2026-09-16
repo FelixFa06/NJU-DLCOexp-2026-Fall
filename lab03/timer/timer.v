28ff95343f903b0b8895f67232424cb8bcb6bb55
FelixFa06 <felfa1414666@gmail.com>
2026-09-16 11:03:21 +0800

    fix: 调整清零信号rst的优先级至最高

--- lab03/timer/timer.v
+++ lab03/timer/timer.v
@@ -7,21 +7,17 @@ module timer(
 );
 
     always @(posedge clk)
-        if (en) begin
-            if (rst) begin
+        if (rst) begin
+            Q <= 0;
+            rco <= 1'b0;
+        end
+        else if (en)
+            if (Q==59) begin
                 Q <= 0;
-                rco <= 1'b0;
+                rco <= 1'b1;
             end
             else begin
-                if (Q==59) begin
-                    Q <= 0;
-                    rco <= 1'b1;
-                end
-                else begin
-                    Q <= Q+1;
-                    rco <= 1'b0;
-                end
+                Q <= Q+1;
+                rco <= 1'b0;
             end
-        end
-
 endmodule
