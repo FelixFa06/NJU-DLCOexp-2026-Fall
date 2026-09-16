eddc4b8ee6cb4fd467f6b4db1843aeb1e9587b1a
FelixFa06 <felfa1414666@gmail.com>
2026-09-16 10:46:29 +0800

    fix: fix the bug of number 9 display in bcd7seg.v

--- lab01/exp2/exp2.v
+++ lab01/exp2/exp2.v
@@ -39,7 +39,6 @@ module bcd7seg(
     input  [3:0] b,
     output reg [6:0] h
 );
-
     always @(*) begin
         case (b)
             4'd0:    h = 7'b1000000;
@@ -51,9 +50,8 @@ module bcd7seg(
             4'd6:    h = 7'b0000010;
             4'd7:    h = 7'b1111000;
 			4'd8:    h = 7'b0000000;
-			4'd9:	 h = 7'b0000100;
+			4'd9:	 h = 7'b0010000;
             default: h = 7'b1111111;
         endcase
     end
-
 endmodule
