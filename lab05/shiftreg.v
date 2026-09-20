7225eefdd8f77e66f26218f4479f920ccbf1fbd7
FelixFa06 <felfa1414666@gmail.com>
2026-09-20 10:48:56 +0800

    feat: add shiftreg.v for OJ

---
+++ lab05/shiftreg.v
@@ -0,0 +1,23 @@
+module shiftreg(
+    input  [7:0]  din,
+	input  clk,
+	input  [2:0] func,
+	input  lin,
+	output reg [7:0] dout
+);
+		
+    always @(posedge clk) begin
+        case (func)
+            3'b000: dout <= 8'd0;
+            3'b001: dout <= din;
+            3'b010: dout <= {1'b0, dout[7:1]};
+            3'b011: dout <= {dout[6:0], 1'b0};
+            3'b100: dout <= {dout[7], dout[7:1]};
+            3'b101: dout <= {lin, dout[7:1]};
+            3'b110: dout <= {dout[0], dout[7:1]};
+            3'b111: dout <= {dout[6:0], dout[7]};
+            default: ;
+        endcase
+    end
+
+endmodule
